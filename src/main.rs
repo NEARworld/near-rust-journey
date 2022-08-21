5eb0626d3c7718ad018f2063e233516c256c1cf4
NEARworld <artemismars2@outlook.com>
2022-08-21 17:39:55 +0900

    make a construct message

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io;
+use std::{thread, time};
+use std::io::{self, Write};
 use rand::Rng;
 
 enum Habitat {
@@ -10,23 +11,43 @@ struct Planet {
     habitat: Habitat,
     name: String
 }
-
 impl Planet {
-    fn is_habitable(self: &Self) -> bool {
+    fn is_habitable(self) -> bool {
         matches!(self.habitat, Habitat::Habitable)
+    }
+}
+enum Spaceship {
+    Mothership,
+    Colonyship
+}
+enum Building {
+    Farm,
+    Factory
+}
+
+fn construct_things() {
+    let half_sec = time::Duration::from_secs_f32(0.5);
+    print!("Constructing");
+    std::io::stdout().flush().unwrap();
+    thread::sleep(half_sec);
+    for n in 1..4 {
+        print!(".");
+        std::io::stdout().flush().unwrap();
+        thread::sleep(half_sec);
     }
 }
 
 fn main() {
 
     println!("::::: Welcome to Space Colony game :::::");
+    thread::sleep(time::Duration::from_secs(1));
     println!("Let's generate your own planet! Write your planet name.");
     let mut planet_name = String::new();
     io::stdin().read_line(&mut planet_name).expect("Something went wrong");
     println!("Ok! Your planet name is <{}>.", planet_name.trim());
 
     // random Habitat variant to the gamer's planet.
-    let num: u32 = rand::thread_rng().gen_range(0..=1);
+    let num: u32 = rand::thread_rng().gen_range(0..=0);
 
     let mother_planet = Planet {
         habitat: match num {
@@ -36,6 +57,13 @@ fn main() {
         },
         name: planet_name
     };
-    
-    println!("result: {}", mother_planet.is_habitable());
+
+    if mother_planet.is_habitable() {
+        println!("Your planet is habitable. People started building a farm in this planet.");
+        construct_things();
+    } else {
+        println!("kek! The planet is uninhabitable. People are trying to build a new colonyship.");
+        construct_things();
+        // println!("People are leaving {}", mother_planet.name);
+    }
 }
